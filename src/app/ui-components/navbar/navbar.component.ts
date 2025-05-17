c46f6ae7146b83c6a9530892f258e2398e551bdb
RyamL1221 <ryaml1221@gmail.com>
2025-05-17 16:35:32 -0400

    fixed navbar

--- src/app/ui-components/navbar/navbar.component.ts
+++ src/app/ui-components/navbar/navbar.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLinkActive } from '@angular/router';
+import { RouterLinkActive, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [ RouterLinkActive],
+  imports: [ RouterLinkActive, RouterModule ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
