142bb7f8f7796b33e3da376c43f016c6f56a464c
RyamL1221 <ryaml1221@gmail.com>
2025-05-17 16:04:53 -0400

    fixed apirul for environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiURL: "https://financial-asset-predictor-backend.onrender.com"
+    apiUrl: "https://financial-asset-predictor-backend.onrender.com"
 };
