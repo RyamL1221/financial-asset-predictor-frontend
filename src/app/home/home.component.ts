e285e6bb678fca6475f672e35e8252d160176637
RyamL1221 <ryaml1221@gmail.com>
2025-05-17 16:30:51 -0400

    added homage page and navbar, but navbar does not work

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from "../ui-components/navbar/navbar.component";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [FormsModule, NavbarComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
---
+++ src/app/stock-analyzer/stock-analyzer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-analyzer',
+  standalone: true,
+  imports: [],
+  templateUrl: './stock-analyzer.component.html',
+  styleUrl: './stock-analyzer.component.css'
+})
+export class StockAnalyzerComponent {
+
+}
