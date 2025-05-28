2e660faab9d3eccee72cc4875a9d2a39a2c12587
Connor <connorl343@gmail.com>
2025-05-27 21:04:11 -0400

    Initilize login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { StockAnalyzerComponent } from './stock-analyzer/stock-analyzer.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full'},
   { path: 'home', component: HomeComponent},
-  { path: 'stock-analyzer', component: StockAnalyzerComponent}
+  { path: 'stock-analyzer', component: StockAnalyzerComponent},
+  { path: 'login', component: LoginComponent}
 ];
